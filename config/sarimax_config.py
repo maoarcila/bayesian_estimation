eaf5d389d940bfc1d6cfd47fc14123e672783168
Andres Arcila <42978896+maoarcila@users.noreply.github.com>
2023-07-31 13:02:55 -0400

    fixing config

--- config/sarimax_config.py
+++ config/sarimax_config.py
@@ -2,6 +2,21 @@ import pandas as pd
 
 class sarimax_config(Config):
 
-    p = 3
-    q = 2
-    I = 1
+    def __init__(self) -> None:
+
+        self.start_p = 3,
+        self.start_q = 3,
+        self.adf = "adf"
+        self.maximum_p = 5,
+        self.maximum_q = 5,  # maximum p and q
+        self.series_frequency = 12,  # frequency of series, 12=monthly
+        self.differencing_operations = None,  # 0 if stationary, None -> let model determine 'd',which will be slow
+        self.seasonal = 1,
+        self.seasonal_p = 12,  # Seasonal P
+        self.seasonal_q = 12,  # Seasonal Q
+        self.seasonal_d = 1,  # Seasonal D
+        self.trace = True,
+        self.error_action = None,
+        self.suppress_warnings = True,
+        self.stepwise = True,
+        self.random_state = True,
